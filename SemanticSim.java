03b92f0bc23d8aa1ef7986af406ecca068fb626b
rfishe02 <rfishe02@g.uafs.edu>
2019-06-14 00:39:38 -0500

    more edits

--- SemanticSim.java
+++ SemanticSim.java
@@ -1,10 +1,49 @@
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class SemanticSim {
 
     public static void main(String[] args) {
 
         
 
+    }
+    
+    public static void buildTermContextMatrix(String document) {
+        
+        // Use HashSet to find duplicates?
+        // For each token, word --> col index 
+        //                 cword --> row index
+        
+        try {
+        
+            BufferedReader br = new BufferedReader(new FileReader(document));
+            String[] spl;
+            String read;
+            
+            while((read=br.readLine())!=null) {
+                
+            }
+            
+            br.close();
+        
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        
+    }
+    
+    public static void calculateSimilarity() {
+        
+    
+    }
+    
+    public static void getContext() {
+        
+    
     }
 
 }
