bff16d0a530a527a97fb76c959724cfab63e514a
rfishe02 <rfishe02@g.uafs.edu>
2019-06-16 09:53:31 -0500

    built matrix

--- SemanticSim.java
+++ SemanticSim.java
@@ -3,82 +3,104 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
-import java.util.Queue;
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class SemanticSim {
 
     public static void main(String[] args) {
     
-        //buildTermContextMatrix(args[0]);
-        
-        String[] prev = {"this","is","a"};
-        String[] read = {"test","its","just"};
- 
-        int window = 3;
-        int b = prev.length-window;
+        buildTermContextMatrix(args[0]);
 
-        for(int a = 1; a <= window; a++) {
-            
-            for(int i = 0; i < prev.length; i++) {
-                
-                if(i < prev.length-a) {
-                    System.out.println(prev[i]+" "+prev[i+a]);
-                }
-                
-                if(i >= b && a <= window) {
-                    System.out.println(prev[i]+" "+read[a-1]);
-                }
-            
-            }  
-            b++;
+    }
     
+    public static HashMap<String,Integer> getVocab(String filename) {
+        HashMap<String, Integer> vocab = new HashMap<>();
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String[] spl;
+            String read;
+            int col = 0;
+        
+            while((read = br.readLine())!=null) {
+                spl = read.split(" ");
+            
+                for(String s : spl) {
+                    if(!vocab.containsKey(s)) {
+                        vocab.put(s,col);
+                        col++;
+                    } 
+                }
+            
+            }
+        } catch(IOException ex) {
+        
         }
-
-
+        
+        return vocab;
     }
     
     public static void buildTermContextMatrix(String document) {
         
-        // Use HashSet to find duplicates?
-        // For each token, word --> col index 
-        //                 cword --> row index
-        
-        // Arrange in alphabetical order, then use binary search.
-        
-        // Get a cound of distinct words, decide matrix size.
-        
-        // Represent words differently.
-        
-        // Hashmap --> fill maxtrix
-        
-        // Need Pr(Word), Pr(w1, w2), total
-        
-        //HashMap<String,Integer> wordIndex = new HashMap<>();
-        int window = 3;
-        
         try {
         
+            HashMap<String,Integer> wordIndex = getVocab(document);
             BufferedReader br = new BufferedReader(new FileReader(document));
+            int[][] termContentMatrix = new int[wordIndex.size()][wordIndex.size()];
             String[] spl;
+            String[] prev = null;
             String read;
-            int col = 0;
             
             while((read=br.readLine())!=null) {
                 spl = read.split(" ");
                 
+                if(prev != null) {
+                    countTerms(wordIndex,termContentMatrix,prev,spl);
+                }
                 
-                
+                prev = spl;
             }
+            
+            countTerms(wordIndex,termContentMatrix,prev,null);
             
             br.close();
             
+            printContextMatrix(wordIndex,termContentMatrix);
         
         } catch(IOException ex) {
             ex.printStackTrace();
             System.exit(1);
         }
         
+    }
+    
+    public static void countTerms(HashMap<String,Integer> wordIndex, int[][] termContentMatrix, String[] prev, String[] spl) {
+    
+        int window = 2;
+        int b = prev.length-window;
+                    
+        for(int a = 1; a <= window; a++) {
+            
+            for(int i = 0; i < prev.length; i++) {
+                
+                if(i < prev.length-a) {
+                    termContentMatrix[wordIndex.get(prev[i])][wordIndex.get(prev[i+a])] +=1 ;
+                    termContentMatrix[wordIndex.get(prev[i+a])][wordIndex.get(prev[i])] +=1 ;
+                    //System.out.println(prev[i]+" "+prev[i+a]);
+                }
+                
+                if(i >= b && a <= window && spl != null) {
+                    termContentMatrix[wordIndex.get(prev[i])][wordIndex.get(spl[a-1])] +=1 ;
+                    termContentMatrix[wordIndex.get(spl[a-1])][wordIndex.get(prev[i])] +=1 ;
+                    //System.out.println(prev[i]+" "+spl[a-1]);
+                }
+            
+            }  
+                b++;
+        }
+    
     }
     
     public static void calculateSimilarity() {
@@ -88,6 +110,41 @@ public class SemanticSim {
     
     public static void getContext() {
         
+    
+    }
+    
+    public static void printContextMatrix(HashMap<String,Integer> mp, int[][] matrix) {
+    
+        int colNum;
+        System.out.printf("%10s ","");
+    
+        Iterator col = mp.entrySet().iterator();
+        while(col.hasNext()) {
+            Map.Entry cPair = (Map.Entry)col.next();
+            System.out.printf("%8s ",cPair.getKey());
+        }
+        System.out.println();
+
+        Iterator row = mp.entrySet().iterator();
+        while(row.hasNext()) {
+            Map.Entry rPair = (Map.Entry)row.next();
+            
+            col = mp.entrySet().iterator();
+            colNum = 0;
+            while(col.hasNext()) {
+            
+                if(colNum == 0) {
+                    System.out.printf("%10s ",rPair.getKey());
+                }
+            
+                Map.Entry cPair = (Map.Entry)col.next();
+                System.out.printf("%8d ",matrix[(int)rPair.getValue()][(int)cPair.getValue()]);
+                
+                colNum++;
+            }
+            System.out.println();
+            
+        }
     
     }
 
