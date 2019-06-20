b6bf284887ab686b1051465452bce7671a873673
rfishe02 <rfishe02@g.uafs.edu>
2019-06-19 20:12:16 -0500

    edits

--- SemanticSim.java
+++ SemanticSim.java
@@ -7,20 +7,21 @@ import java.util.Comparator;
 import java.util.TreeSet;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
+import java.util.HashMap;
 
 public class SemanticSim {
 
-    static ArrayList<String> vocab;
-    static boolean debug = false;
+    static HashMap<String,Integer> vocab;
     static int window = 3;
 
     public static void main(String[] args) {
         
         float[][] tcm = buildTermContextMatrix(args[0]);
         
-        int u = wordSearch(vocab,"cake");
+        int u = vocab.get("sugar");
         
         getContext(tcm,5,u);
+        
 
     }
     
@@ -67,14 +68,12 @@ public class SemanticSim {
             }
             
             countTerms(vocab,tcm,window,prev,next,sum);
-            
-            weightTerms(tcm,sum);
  
-            if(debug) {
-                printContextMatrix(vocab,tcm);
-                //printSums(vocab,tcm,sum);
-            }
+            weightTerms(tcm,sum);
             
+            //printContextMatrix(vocab,tcm);
+            //printSums(vocab,tcm,sum);
+ 
             br.close();
         
         } catch(IOException ex) {
@@ -84,51 +83,32 @@ public class SemanticSim {
         
         return tcm;
     }
-    
-    static class VocabComparator implements Comparator<String> {
-        public int compare(String s1, String s2) {
-            return s1.compareTo(s2);
-        }
-    }
-    
+ 
     /** Pre-reads the file to obtain |V|. The vocab will be used to
         find the location of terms in the matrix.
-        
-        Uses a TreeMap to arrange distinct terms in order, 
-        which are eventually copied into an ArrayList.
-        
-        The first while loop runs in t time, where t represents tokens.
-        The add method for the TreeSet takes log(|V|) time.
-        
-        The second while loop takes |V| time.
-        
-        Altogether, it takes O( |V| + t log (|V|) ) time.
     */
     
-    public static ArrayList<String> getVocab(String filename) {
-        TreeSet<String> set;
-        ArrayList<String> vocab = null;
-        BufferedReader br;
-        
+    public static HashMap<String,Integer> getVocab(String filename) {
+        HashMap<String,Integer> vocab = null;
+  
         try {
-            br = new BufferedReader(new FileReader(filename));
-            set = new TreeSet<>(new VocabComparator());
-            vocab = new ArrayList<>(set.size());
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            vocab = new HashMap<>();
             String[] spl;
             String read;
-            int col = 0;
+            int key = 0;
         
             while((read = br.readLine())!=null) {
                 spl = read.split(" ");
             
                 for(String s : spl) {
-                    set.add(s);
+                    
+                    if(!vocab.containsKey(s)) {
+                        vocab.put(s,key);
+                        key++;
+                    }
+                    
                 }
-            }
-            
-            Iterator<String> it = set.iterator();
-            while(it.hasNext()) {
-                vocab.add(it.next());
             }
             
         } catch(IOException ex) {
@@ -156,36 +136,33 @@ public class SemanticSim {
     
     */
     
-    public static void countTerms(ArrayList<String> vocab, float[][] tcm, int window, String[] prev, String[] next, int[] sum) {
+    public static void countTerms(HashMap<String,Integer> vocab, float[][] tcm, int window, String[] prev, String[] next, int[] sum) {
         int b = 0;
-        int w1;
-        int w2;
-    
+        int w1, w2;
+        
         for(int a = 0; a < window; a++) {
             
             for(int i = 0; i < prev.length-a; i++) {
-                 
+                
                 if(i < prev.length-(a+1)) {
                 
-                    w1 = wordSearch(vocab,prev[i]);
-                    w2 = wordSearch(vocab,prev[i+(a+1)]);
+                    w1 = vocab.get(prev[i]);
+                    w2 = vocab.get(prev[i+(a+1)]);
                 
                     tcm[ w1 ][ w2 ] ++ ;
                     tcm[ w2 ][ w1 ] ++ ;
                     
-                    sum[ w1 + 1 ] ++;
+                    sum[ w1 + 1 ] ++; // Count sum, which is used to weight terms.
                     sum[ w2 + 1 ] ++;
                     sum[0] += 2;
                     
-                    if(debug) {
-                        System.out.println(i+": "+prev[i]+" "+(i+(a+1))+": "+prev[i+(a+1)]);
-                    }
+                    //System.out.println(i+": "+prev[i]+" "+(i+(a+1))+": "+prev[i+(a+1)]);
                 }
 
                 if(prev[i+b] != null && next[a] != null) {
                 
-                    w1 = wordSearch(vocab,prev[i+b]);
-                    w2 = wordSearch(vocab,next[a]);
+                    w1 = vocab.get(prev[i+b]);
+                    w2 = vocab.get(next[a]);
                 
                     tcm[ w1 ][ w2 ] ++ ;
                     tcm[ w2 ][ w1 ] ++ ;
@@ -194,70 +171,38 @@ public class SemanticSim {
                     sum[ w2 + 1 ] ++;
                     sum[0] += 2;
                     
-                    if(debug) {
-                        System.out.println((i+b)+": "+prev[i+b]+" "+a+": "+next[a]);
-                    }
+                    //System.out.println((i+b)+": "+prev[i+b]+" "+a+": "+next[a]);
                 }
             }
-            if(debug) {
-                System.out.println();
-            }
-            
             b+=1;
         }
     }
     
-    /** Use a binary search to get the index of a word. This is an attempt to
-    avoid storing unnecessary integers to refer to a column. */
+    /** 
     
-    public static int wordSearch(ArrayList<String> vocab, String target) {
-        int ind = -1;
-        int l = 0;
-        int m;
-        int r = vocab.size()-1;
-        
-        while(l <= r) {
-        
-            m = (l+r)/2;
-            
-            if(target.compareTo(vocab.get(m)) > 0) {
-                l = m+1;
-            } else if(target.compareTo(vocab.get(m)) < 0) {
-                r = m-1;
-            } else {
-                return m;
-            }
-        
-        }
-        
-        return ind;
-    }    
-
-    /** Uses PPMI to weight the terms of the term-context matrix. */
+    Uses PPMI to weight the terms of the term-context matrix. 
+    
+    */
     
     public static void weightTerms(float[][] tcm, int[] sum) {
         double val;
 
-        for(int i = 0; i < tcm.length; i++) {
+        for(int row = 0; row < tcm.length; row++) {
         
-            for (int j = 0; j < tcm[0].length; j++) {
-                val = ( (double)tcm[i][j]/sum[0] ) 
-                / ( ( (double)sum[i+1]/sum[0] ) * ( Math.pow(sum[j+1],0.75)/Math.pow(sum[0],0.75) ) );
+            for (int col = 0; col < tcm[0].length; col++) {
+                val = ( (double)tcm[row][col] / sum[0] ) 
+                / ( ( (double)sum[row+1] / sum[0] ) * ( Math.pow(sum[col+1],0.75) / Math.pow(sum[0],0.75) ) );
                 
                 if(val > 0.0) {
                     val = Math.log(val) / Math.log(2);
                 }
                 
                 val = Math.max(val,0);
-                tcm[i][j] = (float)val;
+                tcm[row][col] = (float)val;
                 
-                if(debug) {
-                    System.out.printf("%2.2f ",tcm[i][j]);
-                }
+                //System.out.printf("%2.2f ",tcm[row][col]);
             }
-            if(debug) {
-                System.out.println();
-            }
+            //System.out.println();
         }
     }
     
@@ -279,35 +224,9 @@ public class SemanticSim {
         return (float)((tot) / (Math.sqrt(one) * Math.sqrt(two)));
     }
     
-    static class ContextComparator implements Comparator<ResultObj> {
-        public int compare(ResultObj s1, ResultObj s2) {
-            if(s1.score > s2.score) {
-                return -1;
-            } else if(s1.score < s2.score) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
-    }
-    
-    static class ResultObj {
-    
-        String word;
-        float score;
-        
-        public ResultObj(String word, float score) {
-            this.word = word;
-            this.score = score;
-        }
-    
-    }
-    
     /** Use a priority queue to find the top ten most similiar words. */
-    
-    // Could I get a better runtime wit a graph search?
-    
-    public static String[] getContext( float[][] tcm, int k, int u) {
+
+    public static String[] getContext(float[][] tcm, int k, int u) {
         
         PriorityQueue<ResultObj> pq = new PriorityQueue(new ContextComparator());
         String[] res = new String[k];
@@ -322,9 +241,9 @@ public class SemanticSim {
         }
     
         return res;
-    }
+    } 
 
-    /** Print the matrix to the console. */
+    /** Print the contents of the matrix to the console. */
     
     public static void printContextMatrix(ArrayList<String> vocab, float[][] matrix) {
     
@@ -353,6 +272,8 @@ public class SemanticSim {
         }
     }
     
+    /** Print the contents of the sums array. */
+    
     public static void printSums(ArrayList<String> vocab, float[][] tcm, int[] sum) {
     
         for(int i = 0; i < vocab.size(); i++) {
@@ -367,60 +288,37 @@ public class SemanticSim {
         }
     }
     
-    /*
-    public static void printContextMatrix(HashMap<String,Integer> wordIndex, float[][] matrix) {
-        Iterator row = wordIndex.entrySet().iterator();
-        Iterator col = wordIndex.entrySet().iterator();
-        Map.Entry rPair;
-        Map.Entry cPair;
-        int colNum;
-        
-        System.out.printf("%10s ","");
-         
-        while(col.hasNext()) {
-            cPair = (Map.Entry)col.next();
-            System.out.printf("%8s ",cPair.getKey());
-        }
-        System.out.println();
-
-        while(row.hasNext()) {
-            rPair = (Map.Entry)row.next();
-            
-            col = wordIndex.entrySet().iterator();
-            colNum = 0;
-            while(col.hasNext()) {
-            
-                if(colNum == 0) {
-                    System.out.printf("%10s ",rPair.getKey());
-                }
-            
-                cPair = (Map.Entry)col.next();
-                System.out.printf("%8.2f ",matrix[(int)rPair.getValue()][(int)cPair.getValue()]);
-                
-                colNum++;
-            }
-            System.out.println();
+    //--------------------------------------------------------
+    // CUSTOM CLASSES
+    
+    static class VocabComparator implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            return s1.compareTo(s2);
         }
     }
     
-    public static void printSums(HashMap<String,Integer> wordIndex, float[][] tcm, int[] sum) {
-        Iterator row = wordIndex.entrySet().iterator();
-        Iterator col = wordIndex.entrySet().iterator();
-        Map.Entry rPair;
-        Map.Entry cPair;
-        
-        while(row.hasNext()) {
-            rPair = (Map.Entry)row.next();
-            
-            col = wordIndex.entrySet().iterator();
-            while(col.hasNext()) {
-            
-                cPair = (Map.Entry)col.next();
-                System.out.println( cPair.getKey() +" ("+(int)cPair.getValue()+") "+ sum[(int)cPair.getValue()]+ " " + rPair.getKey() +" ("+(int)rPair.getValue()+") "+ tcm[(int)cPair.getValue()][(int)rPair.getValue()] );
-
+    static class ContextComparator implements Comparator<ResultObj> {
+        public int compare(ResultObj s1, ResultObj s2) {
+            if(s1.score > s2.score) {
+                return -1;
+            } else if(s1.score < s2.score) {
+                return 1;
+            } else {
+                return 0;
             }
-            System.out.println();
         }
     }
-    */
+    
+    static class ResultObj {
+    
+        String word;
+        float score;
+        
+        public ResultObj(String word, float score) {
+            this.word = word;
+            this.score = score;
+        }
+    
+    }
+    
 }
