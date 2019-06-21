d391b5a8b4bfef532bf57c4a078cc5398a3f4f86
rfishe02 <rfishe02@g.uafs.edu>
2019-06-21 15:04:25 -0500

    semi-final edits

--- SemanticSim.java
+++ SemanticSim.java
@@ -13,29 +13,25 @@ import java.util.Map;
 
 public class SemanticSim {
 
-    static HashMap<String,Integer> vocab;
+    static ArrayList<String> vocab;
     static int window = 3;
 
     public static void main(String[] args) {
         
         float[][] tcm = buildTermContextMatrix(args[0]);
         
-        int u = vocab.get("sweet");
+        int u = wordSearch("sweet");
         
         getContext(tcm,5,u);
-        
 
     }
-    
-    /** This method accepts a corpus with each term listed on a single line. 
+
+    /** 
+        This method accepts a corpus with each term listed on a single line. 
         It packs these terms into segments, which are used to find contextual terms.
         
-        This method takes t time to iterate over tokens.
-
-        This method takes |V| space to store distinct tokens.
-        It also takes |V| x |V| space to store the frequencies.
-        
-        Altogether, it takes |V| + |V| x |V| space.
+        Before it begins, it retreives the vocabulary, which provides the 
+        matrix size and index locations for each term in the document.
     */
     
     public static float[][] buildTermContextMatrix(String document) {
@@ -51,7 +47,7 @@ public class SemanticSim {
             String read;
             int i = 0;
             
-            int[] sum = new int[vocab.size() + 1];
+            int[] sum = new int[vocab.size() + 1]; // Should I store the sum? (What about threading?)
     
             while((read=br.readLine())!=null) {
                 next[i] = read;
@@ -89,29 +85,29 @@ public class SemanticSim {
     /** 
         Pre-reads the file to obtain |V|. The vocab will be used to
         find the location of terms in the matrix.
+        
+        Uses a TreeMap to arrange distinct terms in alphabetic order, 
+        which are later copied into an ArrayList.
+        
+        The wordSearch method uses a binary search on the ArrayList to find
+        an index for a given word.
     */
     
-    public static HashMap<String,Integer> getVocab(String filename) {
-        HashMap<String,Integer> vocab = null;
-  
+    public static ArrayList<String> getVocab(String filename) {
+        
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
-            vocab = new HashMap<>();
-            String[] spl;
+            TreeSet<String> set = new TreeSet<>(new VocabComparator());
+            vocab = new ArrayList<>(set.size());
             String read;
-            int key = 0;
         
             while((read = br.readLine())!=null) {
-                spl = read.split(" ");
+                set.add(read);
+            }
             
-                for(String s : spl) {
-                    
-                    if(!vocab.containsKey(s)) {
-                        vocab.put(s,key);
-                        key++;
-                    }
-                    
-                }
+            Iterator<String> it = set.iterator();
+            while(it.hasNext()) {
+                vocab.add(it.next());
             }
             
         } catch(IOException ex) {
@@ -123,18 +119,9 @@ public class SemanticSim {
     }
 
     /**
-    Use a faux-matrix to count the contextual terms of a segment.
-    It pretends that the given segments are a S x S matrix.
-    
-    The two loops use their variables and offset values
-    to find the contextual terms for each word in the segment. 
-    It also finds contextual terms that overlap between to separate segments.
-    
-    The outer loop takes w time, where w represents the window size.
-    The inner loop takes s time, where s represents the size of a segment.
-    It takes log(|V|) time to find a word in the vocab.
-    
-    Altogether, it takes O( w s log(|V|) ).
+    It uses two loops with variables and offset values to find the 
+    contextual terms for each word in the segment. It also finds the contextual terms 
+    that overlap between to separate segments.
     */
     
     public static void countTerms(float[][] tcm, int window, String[] prev, String[] next, int[] sum) {
@@ -147,8 +134,8 @@ public class SemanticSim {
                 
                 if(i < prev.length-(a+1)) {
                 
-                    w1 = vocab.get(prev[i]);
-                    w2 = vocab.get(prev[i+(a+1)]);
+                    w1 = wordSearch(prev[i]);
+                    w2 = wordSearch(prev[i+(a+1)]);
                 
                     tcm[ w1 ][ w2 ] ++ ;
                     tcm[ w2 ][ w1 ] ++ ;
@@ -162,8 +149,8 @@ public class SemanticSim {
 
                 if(prev[i+b] != null && next[a] != null) {
                 
-                    w1 = vocab.get(prev[i+b]);
-                    w2 = vocab.get(next[a]);
+                    w1 = wordSearch(prev[i+b]);
+                    w2 = wordSearch(next[a]);
                 
                     tcm[ w1 ][ w2 ] ++ ;
                     tcm[ w2 ][ w1 ] ++ ;
@@ -179,11 +166,38 @@ public class SemanticSim {
         }
     }
     
-    /** 
+    /**
+    Use a binary search to get the index of a word. This is an attempt to
+    avoid mapping Strings to column values. However, it takes log time rather
+    than constant time.
     
-    Uses PPMI to weight the terms of the term-context matrix. 
-    
+    The program uses the index of the String as the column.
     */
+    
+    public static int wordSearch(String target) {
+        int ind = -1;
+        int l = 0;
+        int m;
+        int r = vocab.size()-1;
+        
+        while(l <= r) {
+        
+            m = (l+r)/2;
+            
+            if(target.compareTo(vocab.get(m)) > 0) {
+                l = m+1;
+            } else if(target.compareTo(vocab.get(m)) < 0) {
+                r = m-1;
+            } else {
+                return m;
+            }
+        
+        }
+        
+        return ind;
+    }  
+
+    /** Uses PPMI to weight the terms of the term-context matrix. */
     
     public static void weightTerms(float[][] tcm, int[] sum) {
         double val;
@@ -211,6 +225,8 @@ public class SemanticSim {
     
     /** Calculate cosine similarity, given two rows from the matrix. */
     
+    // try to use threads here.
+    
     public static float calculateSimilarity( float[][] tcm, int u, int v ) {
         double one = 0.0;
         double two = 0.0;
@@ -225,19 +241,19 @@ public class SemanticSim {
         return (float)((tot) / (Math.sqrt(one) * Math.sqrt(two)));
     }
     
-    /** Use a priority queue to find the top ten most similiar words. */
+    /** 
+    Loop over the vocab, or row of the tcm matrix, to find the most similiar words.
+    It assumes that the ith position of the tcm matrix represents the ith word in the vocab.
+    Uses a priority queue to find the top ten most similiar words. 
+    */
 
     public static String[] getContext(float[][] tcm, int k, int u) {
         PriorityQueue<ResultObj> pq = new PriorityQueue(new ContextComparator());
-        Iterator row = vocab.entrySet().iterator();
-        Map.Entry rPair;
         String[] res = new String[k];
-        String tmp;
-        
-        while(row.hasNext()) {
-            rPair = (Map.Entry)row.next();
-            if((int)rPair.getValue() != u) {
-                pq.add(new ResultObj((String)rPair.getKey(),calculateSimilarity(tcm,u,(int)rPair.getValue())));
+ 
+        for(int i = 0; i < tcm.length; i++) {
+            if(i != u) {
+                pq.add(new ResultObj(vocab.get(i),calculateSimilarity(tcm,u,i)));
             }
         }
         
@@ -283,57 +299,47 @@ public class SemanticSim {
     }
     
     //--------------------------------------------------------
-
-    public static void printContextMatrix(HashMap<String,Integer> wordIndex, float[][] matrix) {
-        Iterator row = wordIndex.entrySet().iterator();
-        Iterator col = wordIndex.entrySet().iterator();
-        Map.Entry rPair;
-        Map.Entry cPair;
-        int colNum;
-        
+    
+    /** Print the contents of the matrix to the console. */
+    
+    public static void printContextMatrix(ArrayList<String> vocab, float[][] matrix) {
+    
         System.out.printf("%10s ","");
-         
-        while(col.hasNext()) {
-            cPair = (Map.Entry)col.next();
-            System.out.printf("%8s ",cPair.getKey());
+        for(String s : vocab) {
+            System.out.printf("%8s ",s);
         }
         System.out.println();
-
-        while(row.hasNext()) {
-            rPair = (Map.Entry)row.next();
-            
-            col = wordIndex.entrySet().iterator();
+        
+        int colNum = 0;
+        
+        for(int i = 0; i < vocab.size(); i++) {
+        
             colNum = 0;
-            while(col.hasNext()) {
+            for(int j = 0; j < vocab.size(); j++) {
             
                 if(colNum == 0) {
-                    System.out.printf("%10s ",rPair.getKey());
+                    System.out.printf("%10s ",vocab.get(i));
                 }
-            
-                cPair = (Map.Entry)col.next();
-                System.out.printf("%8.2f ",matrix[(int)rPair.getValue()][(int)cPair.getValue()]);
-                
                 colNum++;
+                
+                System.out.printf("%8.2f ",matrix[i][j]);
+            
             }
             System.out.println();
         }
     }
     
-    public static void printSums(HashMap<String,Integer> wordIndex, float[][] tcm, int[] sum) {
-        Iterator row = wordIndex.entrySet().iterator();
-        Iterator col = wordIndex.entrySet().iterator();
-        Map.Entry rPair;
-        Map.Entry cPair;
-        
-        while(row.hasNext()) {
-            rPair = (Map.Entry)row.next();
-            
-            col = wordIndex.entrySet().iterator();
-            while(col.hasNext()) {
-            
-                cPair = (Map.Entry)col.next();
-                System.out.println( cPair.getKey() +" ("+(int)cPair.getValue()+") "+ sum[(int)cPair.getValue()+1]+ " " + rPair.getKey() +" ("+(int)rPair.getValue()+") "+ tcm[(int)cPair.getValue()][(int)rPair.getValue()] );
+    /** Print the contents of the sums array. */
 
+    public static void printSums(ArrayList<String> vocab, float[][] tcm, int[] sum) {
+    
+        for(int i = 0; i < vocab.size(); i++) {
+        
+            for(int j = 0; j < vocab.size(); j++) {
+            
+                System.out.println( vocab.get(j) +" ("+j+") " + sum[j+1]
+                + " " + vocab.get(i) +" ("+i+") "+ tcm[i][j] );
+            
             }
             System.out.println();
         }
